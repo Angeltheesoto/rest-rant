d5fdab2818c868f7505bcb776883ce4eda19c815
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2022-07-02 15:45:10 -0400

    Adding places view with mock data

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,24 @@
-const router = require('express').Router()
+const app = require('express').Router()
 
-router.get('/', (req, res) => {
- res.send('GET /places')
+app.get('/', (req, res) => {
+ let places = [{
+  name: 'H-Thai-ML',
+  city: 'Seattle',
+  state: 'WA',
+  cuisines: 'Thai, Pan-Asian',
+  pic: 'https://images.unsplash.com/photo-1615789591457-74a63395c990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmFieSUyMGNhdHxlbnwwfHwwfHw%3D&w=1000&q=80',
+  width: 500,
+  height: 500
+ }, {
+  name: 'Coding Cat Cafe',
+  city: 'Phoenix',
+  state: 'AZ',
+  cuisines: 'Coffee, Bakery',
+  pic: 'https://images.unsplash.com/photo-1615789591457-74a63395c990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmFieSUyMGNhdHxlbnwwfHwwfHw%3D&w=1000&q=80',
+  width: 500,
+  height: 500
+ }]
+ res.render('places/index', { places })
 })
 
-module.exports = router
+module.exports = app
