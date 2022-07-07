bdac1a500fc57483da6909a4325dcdd45d9a2a8a
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2022-07-06 21:35:20 -0400

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 
+// Express Settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
@@ -15,5 +19,6 @@ app.get('*', (req, res) => {
  res.render('error404')
 })
 
+// Listen for Connections
 app.listen(process.env.PORT)
 
--- views/default.jsx
+++ views/default.jsx
@@ -6,8 +6,14 @@ function Def (html) {
   <html>
    <head>
     <title>Title</title>
+    {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" /> */}
+    <link rel="stylesheet" href="/css/style.css" />
+
    </head>
    <body>
+    {/* mycode */}
+    <p><a href="/">Home Page</a></p>
+
     {html.children}
    </body>
   </html>
